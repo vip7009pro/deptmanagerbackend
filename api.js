7ee670965f475117e7d2fef876e5cdff3c24daab
vip7009pro <hung1893@gmail.com>
2024-01-02 11:15:11 +0700

    update api shop

--- api.js
+++ api.js
@@ -55,8 +55,6 @@ function isNumber(str) {
 const queryDB = async (query) => {
   let kq = "";
   try {
-    //await sql.connect(config);    
-    //await sql.connect('Server=192.168.1.2,5005;Database=CMS_VINA;User Id=sa;Password=*11021201$; MultipleActiveResultSets=True; Encrypt=false');   
     const result = await sql.query(query);
     if (result.rowsAffected[0] > 0) {
       if (result.recordset) {
@@ -66,8 +64,7 @@ const queryDB = async (query) => {
       }
     } else {
       kq = { tk_status: "NG", message: "Không có dòng dữ liệu nào" };
-    }
-    //await sql.close();
+    }    
   } catch (err) {
     ////console.log(err);
     kq = { tk_status: "NG", message: err + " " };
@@ -75,30 +72,16 @@ const queryDB = async (query) => {
   return kq;
 };
 exports.checklogin_index = function (req, res, next) {
-  ////console.log("bam login ma cung loi?");
-  try {
-    ////console.log("token client la: " + req.cookies.token);
-    var token = req.cookies.token;
-    //console.log('token= ' + token);
-    //console.log("req", req);
+  try {    
+    var token = req.cookies.token;   
     if (token === undefined)
       token =
         req.body.DATA.token_string === undefined
           ? req.body.token_string
-          : req.body.DATA.token_string;
-    //console.log('req', req);
-    //console.log('token',token);
-    var decoded = jwt.verify(token, "nguyenvanhung");
-    //console.log('decoded',decoded.payload.data[0]);
-    //console.log(decoded);
-    //console.log(decoded["exp"]);
-    var payload_json = decoded.payload.data[0];
-    //console.log('payload_json',payload_json);
-    //console.log(payload_json);
-    //console.log(payload_json[0]);
-    ////console.log('Cookie client = ' + req.cookies.token);
-    req.payload_data = payload_json;
-    //console.log('payload_json',payload_json);
+          : req.body.DATA.token_string;   
+    var decoded = jwt.verify(token, "nguyenvanhung");    
+    var payload_json = decoded.payload.data[0];   
+    req.payload_data = payload_json;  
     if (payload_json.USE_YN === 'N') {
       req.coloiko = "coloi";
     } else {
@@ -112,7 +95,6 @@ exports.checklogin_index = function (req, res, next) {
   }
 };
 exports.checklogin_index_update = function (req, res, next) {
-  ////console.log("bam login ma cung loi?");
   try {
     req.coloiko = "kocoloi";
     next();
@@ -128,15 +110,11 @@ exports.checklogin_login = function (req, res, next) {
     var token = req.cookies.token;
     var decoded = jwt.verify(token, "nguyenvanhung");
     console.log("token= " + token);
-    let payload_json = JSON.parse(decoded["payload"]);
-    ////console.log(payload_json[0]);
-    ////console.log('Cookie client = ' + req.cookies.token);
-    req.payload_data = payload_json[0];
-    //console.log("Di qua check login-login");
+    let payload_json = JSON.parse(decoded["payload"]); 
+    req.payload_data = payload_json[0];  
     res.redirect("/");
     next();
-  } catch (err) {
-    //console.log('Chua dang nhap nen fai ve day ' + err + ' ');
+  } catch (err) {    
     next();
   }
 };
