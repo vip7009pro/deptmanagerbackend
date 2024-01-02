824be14778f8fcd636005c6d387c33ce1e260f74
vip7009pro <hung1893@gmail.com>
2024-01-02 09:49:39 +0700

    update backend

--- api.js
+++ api.js
@@ -4,8 +4,8 @@ const moment = require("moment");
 const { existsSync } = require("fs");
 const fs = require("fs");
 require("dotenv").config();
-const fetch =require('node-fetch');
-let CURRENT_API_URL ='https://script.google.com/macros/s/AKfycbyD_LRqVLETu8IvuiqDSsbItdmzRw3p_q9gCv12UOer0V-5OnqtbJvKjK86bfgGbUM1NA/exec'
+const fetch = require('node-fetch');
+let CURRENT_API_URL = 'https://script.google.com/macros/s/AKfycbyD_LRqVLETu8IvuiqDSsbItdmzRw3p_q9gCv12UOer0V-5OnqtbJvKjK86bfgGbUM1NA/exec'
 function removeVietnameseTones(str) {
   str = str.replace(/à|á|ạ|ả|ã|â|ầ|ấ|ậ|ẩ|ẫ|ă|ằ|ắ|ặ|ẳ|ẵ/g, "a");
   str = str.replace(/è|é|ẹ|ẻ|ẽ|ê|ề|ế|ệ|ể|ễ/g, "e");
@@ -43,11 +43,11 @@ const config = {
   server: process.env.DB_SERVER,
   database: process.env.DB_NAME,
   port: parseInt(process.env.DB_PORT),
-  trustServerCertificate: true, 
-  requestTimeout: 300000, 
+  trustServerCertificate: true,
+  requestTimeout: 300000,
 };
-exports.openConnection = function() {
-   sql.connect(config);   
+exports.openConnection = function () {
+  sql.connect(config);
 }
 function isNumber(str) {
   return /^[0-9]+$/.test(str) && str.length == 4;
@@ -57,7 +57,7 @@ const queryDB = async (query) => {
   try {
     //await sql.connect(config);    
     //await sql.connect('Server=192.168.1.2,5005;Database=CMS_VINA;User Id=sa;Password=*11021201$; MultipleActiveResultSets=True; Encrypt=false');   
-    const result = await sql.query(query);   
+    const result = await sql.query(query);
     if (result.rowsAffected[0] > 0) {
       if (result.recordset) {
         kq = { tk_status: "OK", data: result.recordset };
@@ -87,7 +87,7 @@ exports.checklogin_index = function (req, res, next) {
           ? req.body.token_string
           : req.body.DATA.token_string;
     //console.log('req', req);
-    console.log('token',token);
+    //console.log('token',token);
     var decoded = jwt.verify(token, "nguyenvanhung");
     //console.log('decoded',decoded.payload.data[0]);
     //console.log(decoded);
@@ -98,7 +98,7 @@ exports.checklogin_index = function (req, res, next) {
     //console.log(payload_json[0]);
     ////console.log('Cookie client = ' + req.cookies.token);
     req.payload_data = payload_json;
-    console.log('payload_json',payload_json);
+    //console.log('payload_json',payload_json);
     if (payload_json.USE_YN === 'N') {
       req.coloiko = "coloi";
     } else {
@@ -140,63 +140,63 @@ exports.checklogin_login = function (req, res, next) {
     next();
   }
 };
-exports.process_api = function async (req, res) {
+exports.process_api = function async(req, res) {
   ////console.log(req.files.file);
   //let nhanvien = req.payload_data['EMPL_NO'];
   var qr = req.body;
   let rightnow = new Date().toLocaleString();
-   /* if(req.payload_data['EMPL_NO']!== undefined) console.log(req.payload_data['EMPL_NO']); */
+  /* if(req.payload_data['EMPL_NO']!== undefined) console.log(req.payload_data['EMPL_NO']); */
   console.log(moment().format("YYYY-MM-DD HH:mm:ss") + ":" + qr["command"]);
   if (
     1 /* qr["command"] ==='login' || req.payload_data["EMPL_NO"]==='NHU1903' */
   ) {
-    switch (qr["command"]) {       
+    switch (qr["command"]) {
       case "login":
         (async () => {
           let DATA = qr["DATA"];
           let checkkq = "OK";
           let setpdQuery = `
-            SELECT * FROM U1 WHERE EMAIL='${DATA.EMAIL}' AND UID ='${DATA.UID}'
+            SELECT * FROM U1 WHERE EMAIL='${DATA.EMAIL}' AND PWD ='${DATA.PWD}'
           `;
           console.log(DATA.EMAIL);
-          console.log(DATA.UID);
+          console.log(DATA.PWD);
           //console.log(setpdQuery);
           checkkq = await queryDB(setpdQuery);
           loginResult = checkkq;
-            //console.log("KET QUA LOGIN = " + loginResult);
-            if (loginResult.tk_status != 'NG') {
-              var token = jwt.sign({ payload: loginResult }, "nguyenvanhung", {
-                expiresIn: 3600 * 24 * 100000,
-              });
-              res.cookie("token", token);
-              ////console.log(token);
-              res.send({
-                tk_status: "OK",
-                token_content: token,
-                data: [loginResult],
-              });
-              //console.log('login thanh cong');
-            } else {
-              res.send({ tk_status: "ng", token_content: token });
-              console.log({ tk_status: "ng", token_content: token });
-              //console.log('login that bai');
-            }                         
-        })();                                                           
-        break;      
-        case "signup":
-          (async () => {
-            let DATA = qr["DATA"];
-            //console.log(DATA);           
-            let checkkq = "OK";   
-            let setpdQuery = `
+          //console.log("KET QUA LOGIN = " + loginResult);
+          if (loginResult.tk_status != 'NG') {
+            var token = jwt.sign({ payload: loginResult }, "nguyenvanhung", {
+              expiresIn: 3600 * 24 * 100000,
+            });
+            res.cookie("token", token);
+            ////console.log(token);
+            res.send({
+              tk_status: "OK",
+              token_content: token,
+              data: [loginResult],
+            });
+            //console.log('login thanh cong');
+          } else {
+            res.send({ tk_status: "ng", token_content: token });
+            console.log({ tk_status: "ng", token_content: token });
+            //console.log('login that bai');
+          }
+        })();
+        break;
+      case "signup":
+        (async () => {
+          let DATA = qr["DATA"];
+          //console.log(DATA);           
+          let checkkq = "OK";
+          let setpdQuery = `
             INSERT INTO U1 (UID,EMAIL,PWD, USERNAME,USE_YN,INS_DATE, INS_UID, UPD_DATE, UPD_UID) VALUES ('${DATA.UID}','${DATA.EMAIL}','${DATA.PWD}','${DATA.EMAIL}','Y',GETDATE(),'${DATA.UID}',GETDATE(),'${DATA.UID}')             
             `;
-            console.log(setpdQuery);
-            checkkq = await queryDB(setpdQuery);
-            //console.log(checkkq);
-            res.send(checkkq);                  
-          })();                                                           
-          break;  
+          console.log(setpdQuery);
+          checkkq = await queryDB(setpdQuery);
+          //console.log(checkkq);
+          res.send(checkkq);
+        })();
+        break;
       default:        //console.log(qr['command']);
         res.send({ tk_status: "OK", data: req.payload_data });
     }
